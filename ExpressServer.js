239efff2fca155cfffcb5ac37ce7c419c9957905
Josh Gibb <j_gibb2@mail.fhsu.edu>
2023-05-01 13:29:29 -0400

    First Commit

--- ExpressServer.js
+++ ExpressServer.js
@@ -16,7 +16,7 @@ app.use(logger);
 
 app.use(cors());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
@@ -26,22 +26,15 @@ app.use("/", require("./routes/root"));
 
 app.use("/states", require("./routes/api/states"));
 
-app.all("*", (req, res) =>{
-    res.status(404);
-    if(req.accepts("hmtl")){
-        res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")){
-        res.json({error: "404 Not Found"});
-    } else{
-        res.type("txt").send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.use(errorHandler);
 
-mongoose.connection.once("open", () =>{
-    console.log("Connected to mongoDB");
-    app.listen(port, ()=>{
-        console.log(`Server is listening on port ${port}`);
-    });
+mongoose.connection.once("open", () => {
+	console.log("Connected to mongoDB");
+	app.listen(port, () => {
+		console.log(`Server is listening on port ${port}`);
+	});
 });
